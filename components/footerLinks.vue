ca13d3a745158f2a3203ae6f73ece2a6627632bd
opeyemidy <opeyemidy@gmail.com>
2021-08-20 11:53:02 +0100

    modify form footer

--- components/footerLinks.vue
+++ components/footerLinks.vue
@@ -3,9 +3,13 @@
     <span>Don’t have an account?<nuxt-link to="">Sign up</nuxt-link></span>
     <ul class="info-links">
       <li><nuxt-link to="">© Stripe</nuxt-link></li>
-      <div></div>
+      <li>
+        <div></div>
+      </li>
       <li><nuxt-link to="">Contact</nuxt-link></li>
-      <div></div>
+      <li>
+        <div></div>
+      </li>
       <li><nuxt-link to="">Privacy & terms</nuxt-link></li>
     </ul>
   </div>
